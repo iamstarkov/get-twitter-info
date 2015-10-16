025dfe5f223acac1f617d7a51bc1a07a425d759f
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-10-16 19:53:58 +0200

    even more awesomness by @kinday

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ import Twitter from 'twit';
 
 export default function getTwitterInfo(tokens, username, cb) {
   const client = new Twitter(tokens);
-  client.get('users/show', { screen_name: username }, (err, info) => {
-    cb(err, info);
-  });
+  client.get('users/show', { screen_name: username }, cb);
 };
