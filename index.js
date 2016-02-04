641eefd5b36cbec06bf80c1869a2d0e8fb513771
Vladimir Starkov <iamstarkov@gmail.com>
2016-02-04 11:36:15 +0100

    update indexjs to use twit promises

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import Twitter from 'twit';
-import Promise from 'pinkie-promise';
 
 export default function getTwitterInfo(tokens, username) {
-  const client = new Twitter(tokens);
-
-  return new Promise((resolve, reject) => {
-    const handler = (err, res) => { err ? reject(err) : resolve(res) };
-    client.get('users/show', { screen_name: username }, handler);
-  });
+  return new Twitter(tokens)
+    .get('users/show', { screen_name: username })
+    .then(function(result) { return result.data; });
 };
